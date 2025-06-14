2cc6fd5ef593f2703bf2c82ebbf3d46e612da2ed
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-06-14 19:14:38 +0100

    layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /> @vite('resources/css/app.css')
+    <title>Blade - Auth</title>
+</head>
+
+<body>
+    <main>
+        {{ $slot }}
+    </main>
+
+</body>
+
+</html>
