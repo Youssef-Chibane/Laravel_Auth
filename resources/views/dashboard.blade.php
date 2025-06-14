6cb52fee9e6c76e0caf9b2410b2574896ecc38b1
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-06-14 19:32:55 +0100

    styling dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
-    <h1>Welcom {{ Auth::user()->name }} to the private dashboard</h1>
+    <h1 class="text-center font-bold text-4xl mt-20">Welcom <span class="text-red-500">{{ Auth::user()->name }}</span>
+        to the private dashboard</h1>
 </x-layout>
