b72ef0250d5d10575f12b1358512690c973f8f78
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-06-15 18:38:42 +0100

    render dashboard link if user logged in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,21 @@
         <h1 class="font-bold text-4xl text-gray-800">Hello from Welcome Page</h1>
 
         <div class="space-x-4">
-            <a href="{{ route('login') }}"
-                class="inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-                Login
-            </a>
-            <a href="{{ route('register') }}"
-                class="inline-block px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-                Register
-            </a>
+            @auth
+                <a href="{{ route('dashboard') }}"
+                    class="inline-block px-6 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition">
+                    Dashboard
+                </a>
+            @else
+                <a href="{{ route('login') }}"
+                    class="inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+                    Login
+                </a>
+                <a href="{{ route('register') }}"
+                    class="inline-block px-6 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+                    Register
+                </a>
+            @endauth
         </div>
     </div>
 </x-layout>
