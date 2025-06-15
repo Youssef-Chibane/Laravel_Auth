2430084491d28522efd3b6f8f9a83ceb0ff688b7
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-06-15 18:30:35 +0100

    register view

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,1 +1,42 @@
+<x-layout>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <form method="POST" action="{{ route('register.store') }}"
+            class="bg-white p-6 rounded-xl shadow-md w-full max-w-sm space-y-4">
+            @csrf
 
+            @if ($errors->any())
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <h2 class="text-2xl font-semibold text-center">Login</h2>
+
+            <div>
+                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                <input type="text" name="name" id="name" placeholder="enter your name"
+                    class="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-400">
+            </div>
+
+            <div>
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input type="email" name="email" id="email" placeholder="you@example.com"
+                    class="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-400">
+            </div>
+
+            <div>
+                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••"
+                    class="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:border-blue-400">
+            </div>
+
+            <button type="submit"
+                class="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200">
+                Submit
+            </button>
+        </form>
+    </div>
+</x-layout>
