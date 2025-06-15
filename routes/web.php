30088b3c3ab92167e18d2200738af7afc261f5ac
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-06-15 18:30:48 +0100

    all routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('', 'welcome')->name('welcome');
+Route::view('dashboard', 'dashboard')->middleware('auth')->name('dashboard');
+Route::view('login', 'login')->name('login');
+Route::post('/login', LoginController::class)->name('login-attempt');
+Route::view('register', 'register')->name('register');
+Route::post('/register', RegisterController::class)->name('register.store');
+Route::post('login', LogoutController::class)->name('logout');
